89a0f34c200c8bfebddbd3fe3d5fd5354b3ea02b
valenachegBack <valenacheg@yandex.ru>
2020-04-16 00:54:38 +0500

    Updates

---
+++ precache-manifest.65a59dbc9b2933ee294c00440e5154ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c446bc4187811dda0f51ba9decbd2dff",
+    "url": "/MyBackRepository/index.html"
+  },
+  {
+    "revision": "19d17679d9dec1d1df4f",
+    "url": "/MyBackRepository/static/css/3.b157cdfc.chunk.css"
+  },
+  {
+    "revision": "e8c404ef772a03cfc541",
+    "url": "/MyBackRepository/static/css/4.178eee80.chunk.css"
+  },
+  {
+    "revision": "cf7dc66c5bf9ad1c2168",
+    "url": "/MyBackRepository/static/css/main.b80ee5cb.chunk.css"
+  },
+  {
+    "revision": "4f82e50e7c40824d1f96",
+    "url": "/MyBackRepository/static/js/2.a4d547b2.chunk.js"
+  },
+  {
+    "revision": "811b3d2d36f5d93ac3b9bf1c6d65ac21",
+    "url": "/MyBackRepository/static/js/2.a4d547b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19d17679d9dec1d1df4f",
+    "url": "/MyBackRepository/static/js/3.c5367163.chunk.js"
+  },
+  {
+    "revision": "e8c404ef772a03cfc541",
+    "url": "/MyBackRepository/static/js/4.a3ddcde6.chunk.js"
+  },
+  {
+    "revision": "cf7dc66c5bf9ad1c2168",
+    "url": "/MyBackRepository/static/js/main.8a69e6b3.chunk.js"
+  },
+  {
+    "revision": "8e6f3360e274c4f005d1",
+    "url": "/MyBackRepository/static/js/runtime-main.45540ce1.js"
+  },
+  {
+    "revision": "b8adbec9d887b5b1c05df1b61c00ff93",
+    "url": "/MyBackRepository/static/media/Header_Preloader.b8adbec9.gif"
+  },
+  {
+    "revision": "254ff478ac67ef5357d27d50a330ca7e",
+    "url": "/MyBackRepository/static/media/f5baef4b6b6677020ab8d091ef78a3bc_w200.254ff478.gif"
+  }
+]);
